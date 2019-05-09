00aea427d18a25a8d137ff6df09636c86e52a3d1
mike challis <mike@challis.org>
2019-05-09 17:26:03 +0100

    Authorization changes

---
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =6,ErrorMessage ="Password must be between 6 and 50 charaters long")]
+        public string Password { get; set; }
+    }
+}
---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await this.context.Users.FirstOrDefaultAsync(p => p.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await this.context.Users.AddAsync(user);
+            await this.context.SaveChangesAsync();
+
+            return user;
+        }
+
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await this.context.Users.AnyAsync(p => p.Username == username))
+                return true;
+
+            return false;
+        }
+    }
+}
